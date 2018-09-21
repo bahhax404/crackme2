729c01c24b10a87b9ab31a015a71418e39fa477c
Bahha <bahhamgoune@hotmail.com>
2018-09-21 01:02:20 +0100

    Add files via upload

---
+++ crackme2_keygen.py
@@ -0,0 +1,30 @@
+import string
+username = []
+firstPart = []
+secondPart = []
+x = 0
+username = input("Enter username: ")
+if(len(username)< 5 or len(username) > 9):
+    print("username cannot be less than 4 characters or greater than 9 characters !")
+    exit()
+#the number to shift the second part of the password    
+shiftNumber = len(username) - 1 
+
+for i in username:
+    # increment the ascii characters
+    chars = int(ord(i)) + x  
+    # append the character to firstPart
+    firstPart.append(chr(chars))  
+    x = x + 1
+# reverse the obtained firstPart
+rev = list(reversed(firstPart)) 
+# the final form of the firstPart
+firstPart = ''.join(rev)   
+#generate the second part by shifting character in the end of the username 
+reversedUsername = list(reversed(username))
+#slice username to take only needed charcaters to complete 10 characters password.
+for i in reversedUsername[:10 - len(username)]:
+    chars = int(ord(i)) +  shiftNumber
+    secondPart.append(chr(chars))
+
+print("The password is " + "[" + firstPart + str(''.join(secondPart)) + "]")
